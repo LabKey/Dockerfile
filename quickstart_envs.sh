eb5394651b4df0dd6702e7e446f3bec189732256
Will Mooreston <97046018+labkey-willm@users.noreply.github.com>
2026-01-28 11:30:04 -0800

    fix log4j config logic  (#166)
    * fix log4j config logic to not break when not using json; remove commented out legacy logging options from application.properties; bump to v26.2
    
    * remove rest of legacy logging options from application.properties

--- quickstart_envs.sh
+++ quickstart_envs.sh
@@ -3,7 +3,7 @@
 # example minimal set of environment variables to get started - see readme for additional envs you may wish to set
 
 # embedded tomcat LabKey .jar version to build container with
-export LABKEY_VERSION="26.1"
+export LABKEY_VERSION="26.2"
 
 # minimal SMTP settings
 export SMTP_HOST="localhost"
